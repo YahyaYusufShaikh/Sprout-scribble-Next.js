fe7eb9d549dbf3b2a52b56c08adbe6a78c43f577
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-26 17:35:31 +0530

    Products

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +18,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        className={inter.className}>
+          <nav>
+            <ul className="flex gap-2 justify-between px-4 bg-blue-700">
+              <li>
+                <Link href={"/"}>Home</Link>
+              </li>
+              <li>
+                <Link href={"/about"}>About</Link>
+              </li>
+            </ul>
+          </nav>
+          {children}</body>
     </html>
   );
 }
