c3ccd7f84eb6df949969f56ba48db775a9ba128f
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-03-02 20:14:07 +0530

    neon_db

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,4 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
+const sql = neon(process.env.POSTGRESS_URL as string);
+export const db = drizzle(sql)
