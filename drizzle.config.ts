bc231aaacd47d7fb592664896473ec451c7fea35
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-03-05 18:07:11 +0530

    File

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import type { Config } from 'drizzle-kit';
+import * as dotenv from "dotenv"
+
+dotenv.config({
+    path: ".env.local",
+})
+
+export default  {
+    schema: "./server/schema.ts",
+    out: './server/migrations',
+    dialect: "postgresql", // Replace with the correct dialect for your database
+    dbCredentials: {
+        url: process.env.POSTGRESS_URL!, // Note: connectionString is used instead of url
+    },
+} satisfies Config;
